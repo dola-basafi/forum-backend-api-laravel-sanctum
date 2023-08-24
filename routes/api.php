7ea2e09df62a976dee9b31faed35a1af2a53ae0f
Abdullah <dola.doit@gmail.com>
2023-08-24 11:40:57 +0700

    add router for question

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,19 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/index',[QuestionController::class,'index']);
+Route::get('/detail/{id}',[QuestionController::class,'detail']);
 
-Route::middleware(['auth:sanctum'])->group(function(){
-    Route::get('/logout',[AuthController::class,'logout']);
+Route::middleware(['auth:sanctum'])->group(function () {
+ Route::get('/logout', [AuthController::class, 'logout']);
+ Route::prefix('question')->group(function () {
+  Route::controller(QuestionController::class)->group(function(){
+   Route::post('/create','store');
+   Route::put('/update','update');
+   Route::delete('/delete/{id}','destroy');
+  });
+  
+ });
 });
